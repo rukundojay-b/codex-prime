1eefe981bbe58fac8272b1cd4afde8f47696f570
megacodesdev <megacodesdev3@gmail.com>
2025-04-09 17:14:57 +0200

    Today's session was ended up leaving all user pages designed

--- Backend/routes/contact.js
+++
@@ -1,7 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { sendMessage } = require('../controllers/contactController')
-
-router.post('/', sendMessage)
-
-module.exports = router
